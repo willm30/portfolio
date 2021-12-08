ce36298af21c11a5d04dcc7ae8fff4c3ab19a828
willm30 <64402732+willm30@users.noreply.github.com>
2021-12-09 00:23:16 +0100

    updating gatsby browser

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -2,10 +2,14 @@ import "./src/styles/global.css";
 import React from "react";
 import { FirstRenderProvider } from "./src/context/firstRender";
 import { TabNavigationProvider } from "./src/context/tabNavigation";
+import ReactRecaptcha from "react-recaptcha";
+
 export function wrapRootElement({ element }) {
   return (
-    <FirstRenderProvider>
-      <TabNavigationProvider>{element}</TabNavigationProvider>
-    </FirstRenderProvider>
+    <ReactRecaptcha>
+      <FirstRenderProvider>
+        <TabNavigationProvider>{element}</TabNavigationProvider>
+      </FirstRenderProvider>
+    </ReactRecaptcha>
   );
 }
