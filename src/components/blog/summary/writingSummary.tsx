e25000a74471f38ceae87342697d3af5687e2040
willm30 <64402732+willm30@users.noreply.github.com>
2021-12-08 23:23:26 +0100

    debugging failed static build

--- src/components/blog/summary/writingSummary.tsx
+++ src/components/blog/summary/writingSummary.tsx
@@ -1,9 +1,31 @@
 import React from "react";
 import Summary from "./summary";
 import { useAllWritingPosts } from "../../../utilities/hooks/allWriting";
+import { graphql, useStaticQuery } from "gatsby";
 
 export default function WritingSummary() {
-  const allPosts = useAllWritingPosts().nodes.filter(
+  const { allMdx } = useStaticQuery(
+    graphql`
+      query WritingMdx {
+        allMdx(
+          filter: { slug: { regex: "/writing/" } }
+          sort: { fields: frontmatter___date, order: DESC }
+        ) {
+          nodes {
+            id
+            slug
+            timeToRead
+            frontmatter {
+              title
+              titleLink
+              date(formatString: "DD MMMM YYYY")
+            }
+          }
+        }
+      }
+    `
+  );
+  const allPosts = allMdx.nodes.filter(
     (n) => n.frontmatter.titleLink != "/writing"
   );
 
